4bbe78340411235eee6fe2b8cfa2c952107f720a
lilith <136168186+lilithcyber@users.noreply.github.com>
2025-10-21 02:11:21 +0200

    Don't resolve the saved instance if the server is non sectored

--- Quantumhangar/HangarChecks/Player/PlayerHangar.cs
+++ Quantumhangar/HangarChecks/Player/PlayerHangar.cs
@@ -709,9 +709,18 @@ namespace QuantumHangar.HangarChecks
                 }
                 else
                 {
-                    var serverId = NexusAPI.GetServerIDFromPosition(grid.GridSavePosition);
-                    var serverName = servers[serverId].Name;
-                    sb.AppendLine(" [" + count + "] - ["+ serverName + "] - " + grid.GridName);
+                    var serverType = NexusAPI.GetThisServer().ServerType;
+                    
+                    if (serverType == 0)
+                    {
+                        var serverId = NexusAPI.GetServerIDFromPosition(grid.GridSavePosition);
+                        var serverName = servers[serverId].Name;
+                        sb.AppendLine(" [" + count + "] - ["+ serverName + "] - " + grid.GridName);  
+                    }
+                    else
+                    {
+                        sb.AppendLine($"[{count}] - {grid.GridName}");
+                    }
                 }
 
                 count++;
